8f9fe9df551b442bd2a3c0a63a7cdbb441ed943e
bhartendu ji <bhartendu,ji_cs.aiml24@gla.ac.in>
2025-08-20 17:21:07 +0530

    feat : added divide pens eqally

---
+++ Java-programing-Elements/divide_pens_equally.java
@@ -0,0 +1,48 @@
+public class DividePens {
+    public static void main(String[] args) {
+        int pens = 14;
+        int students = 3;
+
+        int pensPerStudent = pens / students;   // Division
+        int remainingPens = pens % students;    // Modulus
+
+        System.out.println("The Pen Per Student is " + pensPerStudent +
+                           " and the remaining pen not distributed is " + remainingPens);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
