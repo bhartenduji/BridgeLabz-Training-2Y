3245938c2f89f76ab9c4df81320de1902cb7acbc
bhartendu ji <bhartendu,ji_cs.aiml24@gla.ac.in>
2025-08-25 16:28:40 +0530

    Feat: sum of n natural numbers using for loop is added

---
+++ java-control-flow/Level-1/sum-of-n-natural-numbers-using-For-loop.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class SumNaturalFor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a natural number: ");
+        int n = sc.nextInt();
+
+        if (n >= 1) {
+            int sum = 0;
+            for (int i = 1; i <= n; i++) {
+                sum += i;
+            }
+            int formulaSum = n * (n + 1) / 2;
+            System.out.println("Sum using for loop: " + sum);
+            System.out.println("Sum using formula: " + formulaSum);
+            System.out.println("Both results are " + (sum == formulaSum ? "same" : "different"));
+        } else {
+            System.out.println("Not a natural number");
+        }
+
+        sc.close();
+    }
+}
