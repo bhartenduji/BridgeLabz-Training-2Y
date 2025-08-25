5317adafb5a93895ae8ef4782cedcf95211abd03
bhartendu ji <bhartendu,ji_cs.aiml24@gla.ac.in>
2025-08-25 17:09:31 +0530

    Feat : palindrom of number is added

---
+++ java-control-flow/Level-2/palindrom-of-number.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int original = num;
+        int rev = 0;
+
+        while (num != 0) {
+            rev = rev * 10 + num % 10;
+            num /= 10;
+        }
+
+        if (original == rev) {
+            System.out.println(original + " is a Palindrome.");
+        } else {
+            System.out.println(original + " is NOT a Palindrome.");
+        }
+        sc.close();
+    }
+}
