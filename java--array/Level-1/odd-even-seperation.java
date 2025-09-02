a2de2ec04bcf09f14f73ac262be761e1c368dedd
bhartendu ji <bhartendu,ji_cs.aiml24@gla.ac.in>
2025-09-02 17:19:50 +0530

    Feat ; odd even seperation is added

---
+++ java--array/Level-1/odd-even-seperation.java
@@ -0,0 +1,36 @@
+Odd and even numbers separation
+
+import java.util.Scanner;
+
+public class OddEvenSeparator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+        if (number <= 0) {
+            System.out.println("Error: Not a natural number");
+            return;
+        }
+        int size = number / 2 + 1;
+        int[] oddNumbers = new int[size];
+        int[] evenNumbers = new int[size];
+        int oddIndex = 0;
+        int evenIndex = 0;
+        for (int i = 1; i <= number; i++) {
+            if (i % 2 == 0) {
+                evenNumbers[evenIndex++] = i;
+            } else {
+                oddNumbers[oddIndex++] = i;
+            }
+        }
+        System.out.println("Odd numbers: ");
+        for (int i = 0; i < oddIndex; i++) {
+            System.out.print(oddNumbers[i] + " ");
+        }
+        System.out.println("\nEven numbers: ");
+        for (int i = 0; i < evenIndex; i++) {
+            System.out.print(evenNumbers[i] + " ");
+        }
+        input.close();
+    }
+}
