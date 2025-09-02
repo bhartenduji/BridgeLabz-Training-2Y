b9d5aa4dab899679ddd41cb984c957e196e934a1
bhartendu ji <bhartendu,ji_cs.aiml24@gla.ac.in>
2025-09-02 17:16:09 +0530

    Feat ; mean height

---
+++ java--array/Level-1/mean-height.java
@@ -0,0 +1,19 @@
+Mean height of football team
+
+import java.util.Scanner;
+
+public class MeanHeightCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double[] heights = new double[11];
+        double sum = 0.0;
+        for (int i = 0; i < heights.length; i++) {
+            System.out.print("Enter height of player " + (i + 1) + ": ");
+            heights[i] = input.nextDouble();
+            sum += heights[i];
+        }
+        double mean = sum / heights.length;
+        System.out.println("Mean height of the football team: " + mean);
+        input.close();
+    }
+}
