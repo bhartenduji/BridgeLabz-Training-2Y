08a062e823890e8e8439e2f7f56f377e14cca782
bhartendu ji <bhartendu,ji_cs.aiml24@gla.ac.in>
2025-09-02 17:24:35 +0530

    Feat ; fizzbuzz  is added

---
+++ java--array/Level-1/fizzbuzz.java
@@ -0,0 +1,27 @@
+Fizzbuzz Program
+
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+        String[] results = new String[number + 1];
+        for (int i = 0; i <= number; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                results[i] = "FizzBuzz";
+            } else if (i % 3 == 0) {
+                results[i] = "Fizz";
+            } else if (i % 5 == 0) {
+                results[i] = "Buzz";
+            } else {
+                results[i] = String.valueOf(i);
+            }
+        }
+        for (int i = 1; i <= number; i++) {
+            System.out.println("Position " + i + " = " + results[i]);
+        }
+        input.close();
+    }
+}
