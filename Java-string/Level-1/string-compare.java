cef00ce11d0d0b0e0a9be335aca821a7bfdbfd2f
bhartendu ji <bhartendu,ji_cs.aiml24@gla.ac.in>
2025-09-08 00:07:13 +0530

    feat : string compare is added

---
+++ Java-string/Level-1/string-compare.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class StringCompare {
+    public static boolean compareStrings(String a, String b) {
+        if (a.length() != b.length()) return false;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i)) return false;
+        }
+        return true;
+    }
